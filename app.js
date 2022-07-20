0fa9c1f0949e85200e761e8a2201ea759a580943
Rahul Jain <rahulllljainnnn@gmail.com>
2022-07-20 09:25:26 +0530

    adarsh balika update

---
+++ app.js
@@ -0,0 +1,23 @@
+var btntranslate =document.querySelector("#btn-translate");
+var txtinput=document.querySelector("#txt-Input");
+var outputdiv=document.querySelector("#output");
+
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+        return serverURL+"?"+"text"+text
+    }
+ 
+function clickHandler() {
+    var inputText = txtinput . value ;
+
+fetch(getTranslationURL (inputText))
+    .then(response=>response.json())
+    .then(json=> {
+        var transaltedtext= console.log(json.contents.translated);
+        outputdiv.innerText=transaltedtext;
+    })
+};
+
+    btntranslate.addEventListener("click",clickHandler);
