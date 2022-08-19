15a68ff48d05a7a4cbf022d6a6d9dcfb0934f06e
Rahul Jain <rahulllljainnnn@gmail.com>
2022-08-19 15:48:24 +0530

    final update

--- app.js
+++ app.js
@@ -3,22 +3,30 @@ var txtinput=document.querySelector("#txt-Input");
 var outputdiv=document.querySelector("#output");
 
 
-var serverURL="https://api.funtranslations.com/translate/minion.json"
+var serverURL=	"https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text){
-        return serverURL+"?"+"text"+text
+        return serverURL+"?"+"Text="+text;
     }
  
- 
+function errorhandler(error){
+ console.log("error occured",error);
+ alert("something wrong with server! please try again later")
+
+}
+  
 function clickHandler() {
     var inputText = txtinput.value;
+
+
 
 fetch(getTranslationURL (inputText))
     .then(response=>response.json())
     .then(json=> {
-        var transaltedtext= console.log(json.contents.translated);
+        var transaltedtext= json.contents.translated;
         outputdiv.innerText=transaltedtext;
     })
+    .catch(errorhandler)
 };
 
     btntranslate.addEventListener("click",clickHandler);
